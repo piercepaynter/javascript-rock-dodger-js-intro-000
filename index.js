76ce82e2fc04cda3dc9421a0f2bb5c4414ea5ae7
Pierce Paynter <paynterpierce@gmail.com>
2020-07-01 23:25:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ function checkCollision(rock) {
     else if (rockLeftEdge >= dodgerLeftEdge && rockRightEdge <= dodgerRightEdge){
       return true
     }
-    else if (rockLeftEdge >= dodgerRightEdge && rockRightEdge <= dodgerRightEdge){
+    else if (rockLeftEdge >= dodgerRightEdge && rockRightEdge >= dodgerRightEdge){
       return true
     }
     else {
